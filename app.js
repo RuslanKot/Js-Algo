6c315fd839d1291a9fac8565e857a35851d1d7c0
Ruslan <35609502+RuslanKot@users.noreply.github.com>
2020-02-14 19:12:13 +0200

    Added javascript code

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function writeAlgoDemo() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
+    document.write("Side a=" + side + "side b=" + side_b);
+    document.write("Обьем куба" + side_a * side_a * side_a);
+    document.write("Площадь квадарта" + side_a * side_a);
+    document.write("Площадь прямоугольника" + side_a * side_b);
 }
