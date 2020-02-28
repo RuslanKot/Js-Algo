ec902587cbbac2403131e36a799633c95ebab570
Ruslan <35609502+RuslanKot@users.noreply.github.com>
2020-02-28 16:58:32 +0300

    Added

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ function writeAlgoDemo() {
     document.write("Площадь квадрата" + side_a * side_a);
     document.write("Площадь прямоугольника" + side_a * side_b);
 }
+
+function writeAlgoDemo2() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a= " + side_a + "</p><p>Side b= " + side_b + "</p>");
+    document.write("<p>Площадь прямоугольного треугольника " + side_a * side_b / 2 + "</p>");
+}
